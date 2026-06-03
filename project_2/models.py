06ab43c3a74ce2302562601f844e8274ced4654d
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-06-03 15:22:40 +0200

    task 1

--- project_2/models.py
+++ project_2/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Name")
+    description = models.TextField(verbose_name="Description")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
