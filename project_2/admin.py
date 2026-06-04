77a5e91d1ed89d51729e102ca93c2e82349934d9
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-06-04 17:25:49 +0200

    tasks 13

--- project_2/admin.py
+++ project_2/admin.py
@@ -4,11 +4,7 @@ from .models import Project, Task, Tag, ProjectFile
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = [
-        'name',
-        'display_count_of_files',
-        'created_at',
-    ]
+    list_display = ['name', 'display_count_of_files', 'created_at',]
     search_fields = ['name']
 
     def display_count_of_files(self, obj):
@@ -16,25 +12,22 @@ class ProjectAdmin(admin.ModelAdmin):
 
     display_count_of_files.short_description = 'Count of Files'
 
+
+    def replace_spaces_with_underscores(self, request, objects):
+        for obj in objects:
+            obj.name = obj.name.replace(' ', '_')
+            obj.save()
+        return objects
+
+    replace_spaces_with_underscores.short_description = 'Replace spaces with underscores'
+    actions = ['replace_spaces_with_underscores']
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = [
-        'title',
-        'project',
-        'status',
-        'priority',
-        'created_at',
-        'due_date',
-        'assignee'
-    ]
+    list_display = ['title', 'project', 'status', 'priority', 'created_at', 'due_date', 'assignee']
     search_fields = ['title']
-    list_filter = [
-        'status',
-        'priority',
-        'project',
-        'created_at',
-        'due_date'
-    ]
+    list_filter = ['status', 'priority', 'project', 'created_at', 'due_date']
 
 
 @admin.register(Tag)
@@ -44,11 +37,7 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(ProjectFile)
 class ProjectFileAdmin(admin.ModelAdmin):
-    list_display = [
-        'name',
-        'file',
-        'created_at'
-    ]
-
+    list_display = ['name', 'file', 'created_at']
     search_fields = ['name']
     list_filter = ['created_at']
+
